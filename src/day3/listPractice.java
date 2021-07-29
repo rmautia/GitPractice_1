7669cd7fd7875f4d27a9e13d0b153e90625e3efb
rmautia <raphaelnyangenya@gmail.com>
2021-07-29 21:34:18 +0300

    commented grey info

--- src/day3/listPractice.java
+++ src/day3/listPractice.java
@@ -5,5 +5,6 @@ import java.util.ArrayList;
 public class listPractice {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
+        // adding one more item
     }
 }
