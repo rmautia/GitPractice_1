7305030af5f7b55f1f576924ac3c068cf545026c
rmautia <raphaelnyangenya@gmail.com>
2021-07-29 21:46:52 +0300

    More information on the set

--- src/day3/setPractice.java
+++ src/day3/setPractice.java
@@ -3,6 +3,10 @@ package day3;
 public class setPractice {
     public static void main(String[] args) {
     // Set is a collection type that only store unique item
+    // whenever you have a duplicate, it will just remove it.
+    // it's a collection of unique elements.
+
+
 
     }
 }
