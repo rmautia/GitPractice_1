fcaf6404c81d17a2bbb4b512001cfa99fd187d56
rmautia <raphaelnyangenya@gmail.com>
2021-07-29 21:33:45 +0300

    Created a new arraylist called list

--- src/day3/listPractice.java
+++ src/day3/listPractice.java
@@ -1,4 +1,9 @@
 package day3;
 
+import java.util.ArrayList;
+
 public class listPractice {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+    }
 }
